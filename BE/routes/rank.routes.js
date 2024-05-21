79e0091dd3c91c036daa60c43f63b2e3322e9931
emirfateen <emir.fateen@ui.ac.id>
2024-05-22 00:16:41 +0700

    benerin + tambah bySubject

--- BE/routes/rank.routes.js
+++ BE/routes/rank.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const rankController = require('../controllers/rank.controller.js');
 
 router.get('/getRank', rankController.getRank);
-router.get('/rank/:userId', rankController.getUserRank);
+router.get('/getUserRank/:user_id', rankController.getUserRank);
+router.get('/getRankBySubject/:subject_id', rankController.getRankBySubject);
 
 module.exports = router;
